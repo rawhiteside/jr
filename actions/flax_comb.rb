30b3e08193d52243d787258fa2523fd70d911547
Bob <bob.whiteside@gmail.com>
2015-09-13 10:50:58 -0700

    stack of combs

--- actions/flax_comb.rb
+++ actions/flax_comb.rb
@@ -11,27 +11,25 @@ class FlaxComb < Action
     w.refresh
     stat_wait('End')
     w.refresh
-    next if w.click_on('Continue')
-    next if w.click_on('Separate')
-    # This only if there's nothing else. 
-    w.click_on('Clean')
+    # Clean only if there's nothing else. 
+    # Nil if nothing to click on.  The comb crashed.
+    w.click_on('Continue') || w.click_on('Separate') || w.click_on('Clean')
   end
   
   def setup(parent)
     gadgets = [
-      {:type => :point, :label => 'Drag to comb window', :name => 'w'},
+      {:type => :point, :label => 'Drag to comb window\n(or stack of such).', :name => 'w'},
     ]
     @vals = UserIO.prompt(parent, @name, @name, gadgets)
   end
 
   def act
-    w = PinnableWindow.from_point(point_from_hash(@vals, 'w'))
-    unless w
-      UserIO.error('No window found')
-      return
+    loop do
+      w = PinnableWindow.from_point(point_from_hash(@vals, 'w'))
+      break unless w
+      loop { break unless process(w) }
+      w.unpin
     end
-    loop { process(w) }
-
   end
 end
 Action.add_action(FlaxComb.new)
