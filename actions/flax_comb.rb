500cfab1e8417f579c9c7677d4ac71559fdf5e26
Bob <bob.whiteside@gmail.com>
2015-09-12 21:28:59 -0700

    Don't count.  Just do whatever.

--- actions/flax_comb.rb
+++ actions/flax_comb.rb
@@ -7,41 +7,31 @@ class FlaxComb < Action
   end
 
 
-  def process(w, max)
-    count = 0
-    loop do
-      w.refresh
-      stat_wait('End')
-      w.refresh
-      if w.click_on('Continue')
-	# The idea is to quit the loop before
-	# clicking on the next "Clean".
-	count += 1
-	break if count >= max * 2
-	next
-      end
-      next if w.click_on('Separate')
-      w.click_on('Clean')
-    end
+  def process(w)
+    w.refresh
+    stat_wait('End')
+    w.refresh
+    next if w.click_on('Continue')
+    next if w.click_on('Separate')
+    # This only if there's nothing else. 
+    w.click_on('Clean')
   end
   
   def setup(parent)
-    comps = [
+    gadgets = [
       {:type => :point, :label => 'Drag to comb window', :name => 'w'},
-      {:type => :number, :label => 'Number of cycles', :name => 'count'},
     ]
-    @vals = UserIO.prompt(parent, @name, @name, comps)
+    @vals = UserIO.prompt(parent, @name, @name, gadgets)
   end
 
   def act
-    count = @vals['count'].to_i
     w = PinnableWindow.from_point(point_from_hash(@vals, 'w'))
     unless w
       UserIO.error('No window found')
       return
     end
+    loop { process(w) }
 
-    process(w, count)
   end
 end
 Action.add_action(FlaxComb.new)
