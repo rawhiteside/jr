39ff191cff47ff317db57d4f20e4b47745eb49a9
Bob <bob.whiteside@gmail.com>
2015-10-28 12:16:55 -0700

    Pyramid push stack.

---
+++ actions/pyramids.rb
@@ -0,0 +1,43 @@
+require 'action'
+require 'window'
+
+class PyramidPushStack < Action
+  def initialize
+    super('Pyramid push', 'Misc')
+  end
+  def setup(parent)
+    gadgets = [
+      {:type => :point, :label => 'Drag to stack of Rolling blocks', :name => 'win'},
+      {:type => :combo, :label => 'Which direction?', :name => 'direction',
+       :vals => ['North', 'South', 'East', 'West'],
+      },
+    ]
+    @vals = UserIO.prompt(parent, 'block', 'Block', gadgets)
+  end
+
+  def act
+    dir = @vals['direction']
+    while win = PinnableWindow.from_point(point_from_hash(@vals, 'win'))
+      puts "pushing #{dir}"
+      push(win, dir)
+    end
+  end
+
+  def push(win, direction)
+    loop do
+      win.refresh
+      text = win.read_text
+      if text && text.include?(direction)
+        p text
+        stat_wait('End')
+        win.click_on("Push this block #{direction}")
+        sleep_sec 1.0
+      else
+        win.unpin
+        break
+      end
+    end
+  end
+end
+
+Action.add_action(PyramidPushStack.new)
