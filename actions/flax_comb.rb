7ddf25e0040e974f7efaf99c280289e4f3b9f3d9
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2018-03-04 10:32:20 -0800

    Provide a "MAx Clean" setting.

--- actions/flax_comb.rb
+++ actions/flax_comb.rb
@@ -7,35 +7,57 @@ class FlaxComb < Action
   end
 
 
-  def process(w)
-    stat_wait('End')
-    # Clean only if there's nothing else. 
-    # Nil if nothing to click on.  The comb crashed.
-    rv = nil
-    with_robot_lock do
-      rv = w.click_on('Continue') || w.click_on('Separate') || w.click_on('Clean')
-      sleep_sec(0.1)
-      w.refresh
-      sleep_sec(0.1)
-    end
-    rv
+  def process(w, max_clean)
+      clean_count = 0
+      loop do 
+        did_something = nil
+        stat_wait('End')
+        # Clean only if there's nothing else. 
+        # Nil if nothing to click on.  The comb crashed.
+        rv = nil
+        with_robot_lock do
+          w.refresh
+          did_something = nil
+          if w.click_on('Continue') || w.click_on('Separate')
+            did_something = true
+          else
+            break if clean_count >= max_clean
+            if w.click_on('Clean')
+              did_something = true
+              clean_count += 1
+            end
+          end
+          break unless did_something
+          sleep_sec(0.1)
+          w.refresh
+          sleep_sec(0.1)
+        end
+        break unless did_something
+      end
   end
   
   def setup(parent)
     gadgets = [
+      {:type => :number, :label => 'Max count for Clean.', :name => 'count'},
       {:type => :point, :label => 'Drag to comb window (or stack of such).', :name => 'w'},
     ]
     @vals = UserIO.prompt(parent, persistence_name, action_name, gadgets)
   end
 
   def act
+    clean_count = 0
+    max_clean = @vals['count'].to_i
+    # Process the stack of windows.
     loop do
       w = PinnableWindow.from_point(point_from_hash(@vals, 'w'))
       break unless w
-      w.refresh
-      loop { break unless process(w) }
+      process(w, max_clean)
+      # 
+      # Dismiss the box telling me the comb died. 
       p = PopupWindow.find
       p.dismiss if p
+      # 
+      # Lose the done comb.
       w.unpin
     end
   end
