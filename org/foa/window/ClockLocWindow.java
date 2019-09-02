da20dd5eed872b0240275f2e0010359a9e2ab950
dhhale <hunterhale2001@gmail.com>
2019-09-02 16:39:30 -0700

    Clock can now be read (for western desert at least).

--- org/foa/window/ClockLocWindow.java
+++ org/foa/window/ClockLocWindow.java
@@ -25,7 +25,7 @@ public class ClockLocWindow extends AWindow {
 		//New system is 150 wide and 60 tall and not a rectangle but can probably be models as such.
 		//  It is immovable and 30 pixels off the top on my system other milage may vary.
 		System.out.println("Creating new cloc manual window");
-		Rectangle rect = new Rectangle(screenWidth/2 - 100, 37, 200, 39);
+		Rectangle rect = new Rectangle(screenWidth/2 - 110, 37, 220, 39);
 		if (rect == null) {
 			System.out.println("Failed to find clock loc window.");
 			return null;
@@ -39,8 +39,8 @@ public class ClockLocWindow extends AWindow {
     }
 
     public TextReader textReader() {
-	flushTextReader();
-	return super.textReader();
+		flushTextReader();
+		return super.textReader();
     }
 
     public String dateTime() {
@@ -91,11 +91,13 @@ public class ClockLocWindow extends AWindow {
 
     private int[] attemptCoords(String text) {
 		String[] lines = text.split("\n");
-		String position = lines[lines.length - 2];
+		String position = lines[lines.length - 1];
+		System.out.println(position);
 		String[] chunks = position.split(":");
-		String[] words = chunks[1].split(" ");
-		int y = Integer.parseInt(words[words.length - 1]);
-		String xstring = words[words.length - 2].replaceAll(",", "");
+		String[] words = chunks[1].split(",");
+		int y = Integer.parseInt(words[words.length - 1].replaceAll(" ",""));
+
+		String xstring = words[words.length - 2].replaceAll(",", "").replaceAll(" ","");
 		int x = Integer.parseInt(xstring);
 		return new int[] {x, y};
     }
