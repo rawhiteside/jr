0d04f6c5987581d46c7268a20f0fff4e063b6411
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2019-10-05 14:46:38 -0700

    this currently holds stuff for patches.

--- actions/atest.rb
+++ actions/atest.rb
@@ -3,26 +3,67 @@ require 'timer'
 require 'window'
 require 'actions/kettles'
 
-class BuilderTest < Action
+class DefinePatchTest < Action
 
-  def initialize(name = 'Builder: 30 x small rotate')
+  def initialize(name = 'Define patch')
     super(name, 'Test/Dev')
   end
 
   def setup(parent)
-    true
+    gadgets = [
+      {:type => :point, :label => 'Drag top left corner of patch', :name => 'tl'},
+      {:type => :point, :label => 'Drag lower right corner of patch', :name => 'lr'},
+      {:type => :text, :label => 'Name of patch (one word)', :name => 'name'}
+    ]
+    @vals = UserIO.prompt(parent, nil, 'Define patch', gadgets)
+
   end
 
 
   def act
-    b = BuildMenu.new
-    elapsed = Timer.time_this { b.build([:e, :w] * 10)}
-    puts "elapsed: #{elapsed}"
+    x = @vals['tl.x'].to_i
+    y = @vals['tl.y'].to_i
+    width = @vals['lr.x'].to_i - x
+    height = @vals['lr.y'].to_i - y
+    rect = Rectangle.new(x, y, width, height)
+    pb = PixelBlock.new(rect)
+    filename = "patches/#{@vals['name']}.png"
+    pb.save_image(filename)
+    pb_new = PixelBlock.load_image(filename)
+    UserIO.show_image(pb_new, "Image read back.")
+  end
+
+end
+
+Action.add_action(DefinePatchTest.new)
+
+class FindPatchTest < Action
+
+  def initialize(name = 'Find patch')
+    super(name, 'Test/Dev')
+  end
+
+  def setup(parent)
+    gadgets = [
+      {:type => :text, :label => 'Name of patch (one word)', :name => 'name'}
+    ]
+    @vals = UserIO.prompt(parent, nil, 'Patch to find', gadgets)
+  end
+
+
+  def act
+    filename = "patches/#{@vals['name']}.png"
+    pb_patch = PixelBlock.load_image(filename)
+    pb_full = full_screen_capture
+    puts "finding"
+    pt = pb_full.find_patch(pb_patch)
+    p pt.to_s
+    mm(pt)
   end
 end
 
+Action.add_action(FindPatchTest.new)
 
-Action.add_action(BuilderTest.new)
 
 
 class TimeTest < Action
