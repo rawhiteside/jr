cc0ef4c63ebbe7be0455499129252b899eac08a7
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2019-10-05 14:39:01 -0700

    re-work this.  Maybe it's more reliable.

--- actions/kilns.rb
+++ actions/kilns.rb
@@ -12,17 +12,38 @@ class Kilns < GridAction
        {:type => :number, :label => 'Key delay', :name => 'key-delay'}]
   end
 
-  def act_at(p)
-    
-    mm(p['x'],p['y'])
-    sleep_sec 0.1
-    # No delay on the last character.
+  def act
+    delay = @user_vals['delay'].to_f
+    repeat = @user_vals['repeat'].to_i
     str = @user_vals['string']
-    head = str[0, str.size - 1]
-    tail = str[str.size - 1]
-    send_string(head, @user_vals['key-delay'].to_f)
-    send_string(tail)
+    repeat.times do
+      str.each_char do |c|
+        start = Time.now.to_f
+        GridHelper.new(@user_vals, 'g').each_point do |g|
+          with_robot_lock do
+            mm(g['x'],g['y'])
+            sleep_sec 0.1
+	    send_string(c)
+          end
+        end
+
+        wait_more = delay - (Time.now.to_f - start)
+        sleep_sec wait_more if wait_more > 0
+      end
+    end
   end
+
+#  def act_at(p)
+#    
+#    mm(p['x'],p['y'])
+#    sleep_sec 0.1
+#    # No delay on the last character.
+#    str = @user_vals['string']
+#    head = str[0, str.size - 1]
+#    tail = str[str.size - 1]
+#    send_string(head, @user_vals['key-delay'].to_f)
+#    send_string(tail)
+#  end
 end
 
 Action.add_action(Kilns.new)
