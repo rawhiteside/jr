9a1c624683487261eae768ababa7c8bd31d03179
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2019-10-15 22:09:28 -0700

    Works pretty well. Picking stones.

--- actions/pick_stones.rb
+++ actions/pick_stones.rb
@@ -7,22 +7,57 @@ class PickStones < Action
     super("Pick Stones", "Misc")
   end
 
+  def setup(parent)
+    comps = [
+      {:type => :world_loc, :name => 'start', :label => 'Starting coords.'},
+      {:type => :number, :name => 'count', :label => 'Stone coubt before return to start. '},
+    ]
+    @vals = UserIO.prompt(parent, persistence_name, action_name, comps)
+
+    @vals
+  end
+
   def act
+    count_max = @vals['count'].to_i
+    start_coords = WorldLocUtils.parse_world_location(@vals['start'])
+    walker = Walker.new
+    walker.walk_to(start_coords)
+    sleep 1
+    count = 0
+
     loop do
       pt = find_point
-      rclick_at pt unless pt.nil?
-      AWindow.dismissAll if WindowGeom.isRightEdgeBorder(getColor(pt))
-      puts "nothing" if pt.nil?
-      sleep 3
+      if pt.nil?
+        puts "Nothing to pick"
+      else
+        rclick_at(pt, 0.1)
+        sleep 0.1
+        color = getColor(pt)
+        AWindow.dismissAll if WindowGeom.isRightEdgeBorder(color)
+      end        
+      sleep 4
+      if count >= count_max
+        walker.walk_to(start_coords)
+        sleep 0.5
+        count = 0
+      else
+        count += 1
+      end
     end
   end
 
-  def stone_color?(color)
-    r = color.red
-    g = color.green
-    b = color.blue
+  def stone_color?(pb, pt)
 
-    return r > 190 && r < 210 && (r-g).abs < 2 && (r-b).abs < 2
+    [[0, 0], [1, 0], [-1, 0], [0, 1], [0, -1]].each do |delta|
+       color = pb.color(pt.x + delta[0], pt.y + delta[1])
+       r = color.red
+       g = color.green
+       b = color.blue
+
+       return false unless r > 170 && r < 210 && (r-g).abs < 3 && (r-b).abs < 3
+     end
+
+     return true
   end
 
   def find_point
@@ -30,10 +65,9 @@ class PickStones < Action
     center = Point.new(pb.width/2, pb.height/2)
     (pb.height/2).times do |r|
       pts = square_with_radius(center, r)
-      # puts pts
-      pts.each {|pt| return pt if stone_color?(pb.color(pt))}
+      pts.each {|pt| return pt if stone_color?(pb, pt)}
     end
-
+    
     return nil
   end
 
