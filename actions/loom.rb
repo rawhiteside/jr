fd29dbf79b086ca9079b7c477ebf2ef7252d3df4
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2019-12-22 09:00:59 -0800

    restring and reload canvas as well as linen.

--- actions/loom.rb
+++ actions/loom.rb
@@ -12,13 +12,39 @@ class Weave < Action
     @vals = UserIO.prompt(parent, persistence_name, action_name, comps)
   end
 
+
+  def reload(loom)
+    if loom.click_on("Load the Loom with #{reload_what}")
+      HowMuch.max
+    end
+  end    
+
+  def restring(loom)
+    if loom.click_on('Re-String')
+      PopupWindow.dismiss
+      loom.refresh
+      sleep_sec(0.5)
+      if loom.click_on('Load the Loom with Twine')
+        HowMuch.max
+      end
+    end
+  end
+
   def act
     loom = PinnableWindow.from_point(point_from_hash(@vals, 'loom'))
     loop do
       stat_wait('End')
       loom.refresh
       loom.click_on(weave_what)
-      sleep 0.5
+      sleep_sec(0.5)
+
+      loom.refresh
+      restring loom
+      PopupWindow.dismiss
+
+      loom.refresh
+      reload loom
+      PopupWindow.dismiss
     end
   end
 end
@@ -33,38 +59,20 @@ class Linen < Weave
     'Weave Thread into Linen'
   end
 
-
-  def act
-    loom = PinnableWindow.from_point(point_from_hash(@vals, 'loom'))
-    puts loom.read_text
-    loop do
-      stat_wait('End')
-      loom.refresh
-      loom.click_on(weave_what)
-      sleep_sec(0.5)
-      loom.refresh
-      if loom.click_on('Re-String')
-        PopupWindow.dismiss
-        loom.refresh
-        sleep_sec(0.5)
-        if loom.click_on('Load the Loom with Twine')
-          HowMuch.max
-        end
-      end
-
-      loom.refresh
-      if loom.click_on('Load the Loom with Thread')
-	HowMuch.max
-      end
-      PopupWindow.dismiss
-    end
+  def reload_what
+    'Thread'
   end
+
 
 end
 
 class Canvas < Weave
   def initialize
     super('Weave Canvas')
+  end
+
+  def reload_what
+    'Twine'
   end
 
   def weave_what
