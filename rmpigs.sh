c3b120b5294b23e6013fc1b85694e024e6a024c8
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2019-12-30 08:57:45 -0800

    remove pigs

---
+++ rmpigs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm "/cygdrive/c/eGenesis/A Tale in the Desert/egyptc/ega179815eb10121f14dcb76c39555f9ca10730dc14.ega"
+rm "/cygdrive/c/eGenesis/A Tale in the Desert/egyptc/egab75515f8819f22267d6e66748581ed382500345a.ega"
+rm "/cygdrive/c/eGenesis/A Tale in the Desert/egyptc/ega10a4efd3313ca13379da90dceb9e985d32feab5f.ega"
