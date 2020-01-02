fcc251db13b66565974a79e1b84549031617e434
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2020-01-02 09:06:30 -0800

    Inspect spore papers.

---
+++ actions/spores.rb
@@ -0,0 +1,42 @@
+require 'action'
+
+class SporePapers < Action
+
+  def initialize
+    super('Spore papers', 'Misc')
+  end
+
+  def setup(parent)
+    gadgets = [
+      {:type => :point, :label => 'The pinned Inspect button', :name => 'inspect'},
+    ]
+    @vals = UserIO.prompt(parent, persistence_name, action_name, gadgets)
+  end
+
+  def act
+    pt = point_from_hash(@vals, 'inspect')
+    win = PinnableWindow.from_point(pt)
+    loop do
+      stat_wait 'Foc'
+      with_robot_lock do
+        win.click_on('Inspect')
+        sleep 0.5
+        popup = PopupWindow.find
+        text = popup.read_text
+        lines = text.split("\n")
+        break if lines.size < 2
+        popup.click_on(lines[1])
+        sleep 0.5
+        popup.click_on("OK")
+        sleep 0.5
+        
+        result = PopupWindow.find
+        sleep 0.5
+        result.click_on("OK")
+        sleep 5
+      end
+    end
+  end
+end
+Action.add_action(SporePapers.new)
+
