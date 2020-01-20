36cbe4d841a9fd49d9a5dbc2dee8ec79f1a1034d
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2020-01-19 16:43:38 -0800

    new macros.

---
+++ actions/potency.rb
@@ -0,0 +1,73 @@
+class PotencyPush < Action
+  def initialize
+    super('Potency Push', 'Misc')
+  end
+  
+  def setup(parent)
+    gadgets = [
+      {:type => :point, :name => 'kitchen', :label => 'Kitchen'},
+    ]
+    @vals = UserIO.prompt(parent, persistence_name, action_name, gadgets)
+  end
+
+  def act
+    pt = point_from_hash(@vals, 'kitchen')
+    kw = Kitchen.new(pt)
+    kw = Kitchen.from_pinnable(kw)
+
+    loop do
+      kw.clean_pot
+      kw.cook_sink_eat
+
+      # Wait for stats-down.
+      sleep 5 while SkillsWindow.new.stat_boosted?
+    end
+  end
+
+end
+
+class Kitchen < PinnableWindow
+  def initialize(pt)
+    rnew = WindowGeom.rectFromPoint(pt);
+    super(rnew)
+  end
+
+  def self.from_pinnable(w)
+    rect = w.rect
+    pt = Point.new(rect.x + 5, rect.y + 5)
+    Kitchen.new(pt)
+  end
+  
+  # Clean out the pot
+  def clean_pot
+    refresh
+    ConfirmationWindow.yes if click_on 'Clean'
+    sleep 4
+  end
+
+  # Cook the sink food and eat it
+  def cook_sink_eat
+    delay = 4
+    refresh
+    HowMuch.amount(6) if click_on 'Mix/Add Honey'
+    sleep delay
+    refresh
+    HowMuch.amount(1) if click_on 'Mix/Add Coconut'
+
+    sleep delay
+    refresh
+    click_on 'Cook'
+    popup = PopupWindow.find
+    popup.click_on 'OK' if popup
+    sleep delay
+
+    # Eat!
+    refresh
+    click_on 'Enjoy'
+    sleep delay
+  end
+
+end
+
+Action.add_action(PotencyPush.new)
+
---
+++ actions/treatment.rb
@@ -0,0 +1,97 @@
+require 'action'
+
+class Treatment < Action
+  WOOD = 'Wood'
+  METAL = 'Metal'
+  TANK_INFO = {
+    'Wood' => {'prefix' => 'Treat/Treat with '},
+    'Metal' => {'prefix' => 'Dissolve/Dissolve '},
+  }
+  DONE_TEXT = 'Processing Complete'
+
+  def initialize
+    super('Treatment (metal, wood)', 'Buildings')
+  end
+
+  def setup(parent)
+    gadgets = [
+      {:type => :point, :name => 'win', :label => 'Drag to pinned tank window'},
+      {:type => :combo, :name => 'tank', :label => 'Type of treatment',
+       :vals => TANK_INFO.keys,},
+      {:type => :number, :name => 'count', :label => 'Number of batches'},
+      {:type => :text, :label => 'Metal name (for metal only)', :name => 'metal-type'},
+      {:type => :label, :label => 'Recipe is a list of time, ingredient lines.  As in:'},
+      {:type => :label, :label => '  20, Lime'},
+      {:type => :label, :label => '  75, Petroleum'},
+      {:type => :big_text, :label => 'Recipe', :name => 'recipe'},
+    ]
+    @vals = UserIO.prompt(parent, persistence_name, action_name, gadgets)
+  end
+
+  def act
+
+    win = PinnableWindow.from_point(point_from_hash(@vals, 'win'))
+    @tank_type = @vals['tank']
+    @tank_data = TANK_INFO[@tank_type]
+    repeat = @vals['count'].strip.to_i
+    repeat.times { treat_one_batch(win) }
+  end
+
+  def treat_one_batch(win)
+    tank_type = @vals['tank']
+    wait_for_treatment_done(win)
+    take_stuff(win)
+    sleep 2
+    load_stuff(win)
+    sleep 2
+    recipe = @vals['recipe'].split("\n")
+    recipe.each do |line|
+      next if line.strip == ''
+      parts = line.split ','
+      seconds = parts[0].strip
+      ingredient = parts[1].strip
+      add_ingredient(win, seconds, ingredient)
+      sleep 1
+      wait_for_treatment_done(win)
+    end
+  end
+
+  def treating_metal?
+    @tank_type == METAL
+  end
+
+  def take_stuff(win)
+    win.click_on('Take')
+  end
+
+  def add_ingredient(win, seconds, ingredient)
+    win.refresh
+    prefix = @tank_data['prefix']
+    if win.click_on(prefix + ingredient)
+      HowMuch.amount(seconds)
+    else
+      puts "Failed to click on \"#{prefix + ingredient}\""
+    end
+  end
+
+  def wait_for_treatment_done(win)
+    loop do
+      win.refresh
+      text = win.read_text
+      break if text.include?(DONE_TEXT)
+      sleep 3
+    end
+  end
+
+  def load_stuff(win)
+    if treating_metal?
+      metal = @vals['metal-type']
+      HowMuch.max if win.click_on("Load/#{metal}")
+    else
+      HowMuch.max if win.click_on('Load Boards')
+    end
+  end
+
+end
+
+Action.add_action(Treatment.new)
