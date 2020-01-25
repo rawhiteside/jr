da0744dfb794a39a0810dcbaa29f5abf82086cca
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2020-01-24 16:33:07 -0800

    Added FillDistaffs.

--- actions/grid_menus.rb
+++ actions/grid_menus.rb
@@ -21,3 +21,29 @@ class GridMenus < GridAction
 end
 Action.add_action(GridMenus.new)
 
+class FillDistaffs < GridAction
+  def initialize
+    super('Fill Distaffs', 'Buildings')
+  end
+  def get_gadgets
+    super  + [
+      {:type => :checkbox, :label => 'Skip center 4', :name => 'should-skip'}
+    ]
+  end
+  def act_at(p)
+    return if should_skip?(p)
+    win = PinnableWindow.from_screen_click(p['x'], p['y'])
+    if win
+      HowMuch.max if win.click_on('Load')
+    end
+  end
+
+  private
+  def should_skip?(p)
+    return unless @user_vals['should-skip'] == 'true'
+
+    (2..3).cover?(p['ix']) && (3..4).cover?(p['iy'])
+  end
+    
+end
+Action.add_action(FillDistaffs.new)
