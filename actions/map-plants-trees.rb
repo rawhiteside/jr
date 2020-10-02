0324b920d7d914b05872a314f263d7d73e15bbff
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2020-10-02 14:21:47 -0700

    Can export wiki map pins for stuffs.

--- actions/map-plants-trees.rb
+++ actions/map-plants-trees.rb
@@ -19,18 +19,81 @@ class MapPlantsTrees < Action
     @plants = YAML.load_file(@plants_file) if File.exist?(@plants_file)
   end
 
+  RECORD_STUFF = 'Record more trees and plants.'
+  SHOW_LOCATIONS = 'Survey locations.'
+  SHOW_PLANT = 'Locations for a plant.'
+  SHOW_TREE = 'Locations for a tree.'
   def setup(parent)
-    true
+    tree_names = @trees.values.collect {|h| h.keys}.flatten.uniq.sort
+    plant_names = @plants.values.collect {|h| h.keys}.flatten.uniq.sort
+
+    gadgets = [
+      {:type => :combo, :name => 'what', :label => 'Do what:',
+       :vals => [RECORD_STUFF, SHOW_LOCATIONS, SHOW_PLANT, SHOW_TREE],
+      },
+
+      {:type => :combo, :name => 'which-plant', :label => 'Which plant?',
+       :vals => plant_names},
+
+      {:type => :combo, :name => 'which-tree', :label => 'Which tree?',
+       :vals => tree_names}
+    ]
+    @vals = UserIO.prompt(parent, 'Map stuff','Map stuff', gadgets)
   end
 
 
   def act
+    what = @vals['what']
+    if what == RECORD_STUFF
+      record_stuff
+    elsif what == SHOW_LOCATIONS
+      show_locs
+    elsif what == SHOW_TREE
+      show_item(@trees, @vals['which-tree'])
+    elsif what == SHOW_PLANT
+      show_item(@plants, @vals['which-plant'])
+    end
+  end
+    
+  def show_item(hash, item)
+    text = ''
+    hash.each_pair do |coords, item_counts|
+      count = item_counts[item]
+      if count
+        text += "(#{pin_for_count(count)}) #{coords[0]}, #{coords[1]}, #{count} items.\n"
+      end
+    end
+    comps = [
+      {:type => :big_text, :rows => 20, :cols => 60, :value => text, :name => 'text', :label => item}
+    ]
+    UserIO.prompt(nil, nil, 'Locations recorded.', comps)
+  end
+
+  def pin_for_count(count)
+    if count > 0 && count < 9
+      return "Bl#{count}"
+    else
+      return 'Bl9'
+    end
+  end
+
+  def show_locs
+    locs = (@plants.keys + @trees.keys).uniq
+    text = locs.inject("") {|accum, loc| accum + "(Bl0) #{loc[0]}, #{loc[1]}, Point\n"}
+    comps = [
+      {:type => :big_text, :rows => 20, :cols => 60, :value => text, :name => 'text', :label => 'Locations'}
+    ]
+    UserIO.prompt(nil, nil, 'Locations surveyed', comps)
+  end
+
+  def record_stuff
     loop do
       sleep 0.1
       point = mouse_pos
       pt = [point.x, point.y]
       win = PinnableWindow.from_point(pt[0] + 30, pt[1])
       next if win.nil?
+
       text = win.read_text
       next if text.nil?
       line1 = text.split("\n")[0]
@@ -45,8 +108,7 @@ class MapPlantsTrees < Action
         match = line1.match("This (.*) produces")
         if match
           add_entry(@trees, match[1].strip, @trees_file)
-          dismiss_all
-          mm point
+          send_vk VK_ESCAPE
         end
       end
     end
