d59906d0ab59e9f674e0ae02246f48d3db3230a3
DESKTOP-PFC0VS2\Bob <bob.whiteside@gmail.com>
2021-06-05 21:56:17 -0700

    Accept stack of windows.

--- actions/flax_comb.rb
+++ actions/flax_comb.rb
@@ -9,20 +9,26 @@ class FlaxComb < Action
   def setup(parent)
     gadgets = [
       {:type => :number, :label => 'Delay seconds', :name => 'delay'},
-      {:type => :point, :label => 'Drag to comb window.', :name => 'w'},
+      {:type => :point, :label => 'Drag to comb window. (or a stack of them)', :name => 'w'},
     ]
     @vals = UserIO.prompt(parent, persistence_name, action_name, gadgets)
   end
 
   def act
     delay = @vals['delay'].to_i
-    w = PinnableWindow.from_point(point_from_hash(@vals, 'w'))
     loop do
+      w = PinnableWindow.from_point(point_from_hash(@vals, 'w'))
       break unless w
-      w.default_refresh_loc = 'lc'
-      w.refresh
-      w.click_on('Repair') || w.click_on('Continue') || w.click_on('Separate') || w.click_on('Clean')
-      sleep delay
+      loop do
+        w.default_refresh_loc = 'tl'
+        w.refresh
+        w.click_on('Continue') || w.click_on('Separate') || w.click_on('Clean')
+        if w.read_text.include?('Repair')
+          w.unpin
+          break
+        end
+        sleep delay
+      end
     end
   end
 end
