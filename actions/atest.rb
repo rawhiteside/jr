b02f949e1259cd89ba01277394219da76d48991c
rawhiteside <bob.whiteside@gmail.com>
2025-07-11 08:15:22 -0700

    sdf

--- actions/atest.rb
+++ actions/atest.rb
@@ -3,6 +3,163 @@ require 'timer'
 require 'window'
 require 'actions/kettles'
 
+class PatchStats < Action
+  def initialize(name = "Stats for patch")
+    super(name, 'Test/Dev')
+  end
+  def setup(parent)
+    gadgets = [
+      {:type => :label, :label => "Gather ranges."},
+      {:type => :point, :label => 'Drag Top Left of rect', :name => 'origin'},
+      {:type => :number, :label => 'How many rows?', :name => 'rows'},
+      {:type => :number, :label => 'How many columns?', :name => 'cols'},
+      {:type => :number, :label => 'Widen range increment?', :name => 'widen'},
+
+      {:type => :label, :label => "Patch detect."},
+      {:type => :number, :label => 'Match size', :name => 'match_size'},
+      
+    ]
+    @vals = UserIO.prompt(parent, name, 'Define patch', gadgets)
+  end
+
+  def act
+    stats_point = point_from_hash(@vals, 'origin')
+    height = @vals['rows'].to_i
+    width = @vals['cols'].to_i
+    widen = @vals['widen'].to_i
+    match_size = [@vals['match_size'].to_i, 1].max
+    
+    patch = PixelBlock.new(Rectangle.new(stats_point.x, stats_point.y, width, height))
+    rgb_ranges = patch_color_ranges_rgb(patch)
+    p "RGB: #{rgb_ranges}"
+    rgb_ranges = widen_range(rgb_ranges, widen)
+    p "RGB, widened: #{rgb_ranges}"
+    hsb_ranges = patch_color_ranges_hsb(patch)
+    p "HSB: #{hsb_ranges}"
+    puts "HSB volume:#{color_volume(hsb_ranges)}"
+    puts "RGB volume:#{color_volume(rgb_ranges)}"
+    loop do
+      show_matches(rgb_ranges, match_size, "RGB, #{match_size}: #{rgb_ranges}, ")
+      UserIO.info("Go again?")
+      check_for_pause
+    end
+  end
+    
+  def widen_range(r, wide)
+    [
+      (r[0].first - wide)..(r[0].last + wide), 
+      (r[1].first - wide)..(r[1].last + wide), 
+      (r[2].first - wide)..(r[2].last + wide),
+    ]
+  end
+
+  # Tag is either "RGB" or "HSB"
+  def show_matches(ranges, match_size, tag)
+    dim = screen_size
+    h3 = (dim.height/3).to_i
+    w3 = (dim.width/3).to_i
+    rect = Rectangle.new(w3, h3, w3, h3)
+    pb = PixelBlock.new(rect)
+    pb_rv = PixelBlock.new(pb.rect)
+    0.upto(pb.width - match_size) do |x|
+      0.upto(pb.height - match_size) do |y|
+
+        is_match = true
+        0.upto(match_size - 1) do |xoff|
+          0.upto(match_size - 1) do |yoff|
+            if !pixel_match_ranges?(pb.get_pixel(x + xoff, y + yoff), ranges)
+              is_match = false
+              break
+            end
+          end
+          break unless is_match
+        end
+        if is_match
+          pb_rv.set_pixel(x, y, 0xFFFFFF)
+        else
+          pb_rv.set_pixel(x, y, 0)
+        end
+      end
+    end
+    UserIO.show_image(pb_rv, tag)
+  end
+
+  def pixel_match_ranges?(pix, ranges)
+    c = Color.new(pix)
+    vals = [c.red, c.green, c.blue]
+    ranges.each do |r|
+      return nil unless r.include?(vals.shift)
+    end
+
+    return true
+  end
+
+  def to_HSB(pb)
+    pb_new = PixelBlock.new(pb.rect)
+    
+    0.upto(pb.width - 1) do|x|
+      0.upto(pb.height - 1) do|y|
+        c = pb.color(x, y)
+        hsb = Color.RGBtoHSB(c.red, c.green, c.blue, nil)
+        cred = hsb[0] * 255
+        c.green = hsb[1] * 255
+        c.blue = hsb[2] * 255
+        pn_new.set_pixel(x, y, c.rgb)
+      end
+    end
+  end
+
+  def color_volume(cvec)
+    Math.sqrt((cvec[0].last - cvec[0].first + 1)**2 +
+              (cvec[1].last - cvec[1].first + 1)**2 +
+              (cvec[2].last - cvec[2].first + 1)**2)
+  end
+  
+  def patch_color_ranges_hsb(pb)
+    hmin = smin = bmin = 255
+    hmax = smax = bmax = 0
+    0.upto(pb.width-1) do |x|
+      0.upto(pb.height-1) do |y|
+        c = pb.get_color(x, y)
+        hsb = Color.RGBtoHSB(c.red, c.green, c.blue, nil)
+
+        hmin = [255*hsb[0], hmin].min.to_i
+        hmax = [255*hsb[0], hmax].max.to_i
+
+        smin = [255*hsb[1], smin].min.to_i
+        smax = [255*hsb[1], smax].max.to_i
+
+        bmin = [255*hsb[2], bmin].min.to_i
+        bmax = [255*hsb[2], bmax].max.to_i
+      end
+    end
+    return [hmin..hmax, smin..smax, bmin..bmax]
+      
+  end
+  def patch_color_ranges_rgb(pb)
+    rmin = gmin = bmin = 255
+    rmax = gmax = bmax = 0
+    0.upto(pb.width-1) do |x|
+      0.upto(pb.height-1) do |y|
+        color = pb.get_color(x, y)
+        rmin = [color.red, rmin].min
+        rmax = [color.red, rmax].max
+
+        gmin = [color.green, gmin].min
+        gmax = [color.green, gmax].max
+
+        bmin = [color.blue, bmin].min
+        bmax = [color.blue, bmax].max
+      end
+    end
+    return [rmin..rmax, gmin..gmax, bmin..bmax]
+      
+  end
+                        
+end
+Action.add_action(PatchStats.new)
+
+
 class StaticPixelsTest < Action
 
   def initialize(name = 'Static pixels in patch')
